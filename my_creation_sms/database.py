e514c1bec73d70442dbbca761a216c6307009f8f
silambarasan21 <silambarasan180421@gmail.com>
2024-06-02 21:00:43 +0530

    Add files via upload

---
+++ my_creation_sms/database.py
@@ -0,0 +1,5 @@
+# User database
+host = 'localhost'
+user = 'root'
+password = 'Simbu18@'
+database = 'student_management'
---
+++ my_creation_sms/file_creation.py
@@ -0,0 +1,12 @@
+##import pymysql
+##
+##a = pymysql.connect(
+##    host="localhost",
+##    user="root",
+##    password="Simbu18@",
+##    database="student_management"
+##    )
+###to create table
+##mycursor = a.cursor()
+##mycursor.execute("CREATE TABLE student_register(f_name VARCHAR(50),l_name VARCHAR(50),course VARCHAR(50),course_package VARCHAR(50),date VARCHAR(50),age VARCHAR(50),gender VARCHAR(50),birth VARCHAR(50),contact VARCHAR(50),email VARCHAR(50),current_course VARCHAR(50),pending_course VARCHAR(50),completed_course VARCHAR(50),Total_fees VARCHAR(50),paid_fees VARCHAR(50),balance_fees VARCHAR(50))")
+##
